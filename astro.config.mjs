4f131a3bd3434d4e8cc46863b31be069bf8bd234
诸葛子房 <xiaoqiu2017wy@163.com>
2025-06-15 22:12:29 +0800

    feat:cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+/*import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import vercel from "@astrojs/vercel/serverless";
@@ -8,4 +8,15 @@ export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel()
+});
+*/
+
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare'; // ✅ 使用 Cloudflare 适配器
+
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'server', // Cloudflare SSR 需要使用 server 输出
+  adapter: cloudflare(),
 });
